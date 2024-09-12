4cc61d81fad0841c7bab87256374d60d2c19983b
prathik2401 <saiprathik2419@gmail.com>
2024-09-12 19:44:41 +0530

    solved http issue: Mixed contend requested

--- ticketly_backend/ticketly_backend/settings.py
+++ ticketly_backend/ticketly_backend/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-071j*=ng6bsg(xoo8w5pr1oj_5=a%m2hjx2m(^vh9($(5tkapq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://8e1f0b48-e0fb-47a5-9862-833d90622395-prod.e1-eu-north-azure.choreoapis.dev/ticketly/backend/v1.0/', 'https://8e1f0b48-e0fb-47a5-9862-833d90622395-prod.e1-eu-north-azure.choreoapis.dev/ticketly/backend/v1.0/','www.ticketly.live' ,'ticketly.live', 'localhost']
+ALLOWED_HOSTS = ['https://8e1f0b48-e0fb-47a5-9862-833d90622395-prod.e1-eu-north-azure.choreoapis.dev/ticketly/backend/v1.0/','www.ticketly.live' ,'ticketly.live', 'localhost']
 
 
 # Application definition
